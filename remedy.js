38590b81fe713d7f5424811a691693de7463e567
Lucas Cruz Fabiano <lucas.cruz.fabiano@everis.com>
2018-04-28 22:55:28 -0300

    remedy

--- remedy.js
+++ remedy.js
@@ -21,8 +21,13 @@ window.addEventListener('load', function() {
 		$('img[alt="Menú para Mis últimas peticiones"').click(function() {
 			window.setTimeout(function() {
 				$("div.MenuOuter").height(102);
-				console.log($("tbody.MenuTableBody").html());
-				$("tbody.MenuTableBody").html('<tr class="MenuTableRow"><td class="MenuEntryName" nowrap="">Operação | Release em Produção</td><td class="MenuEntryNoSub" arvalue="Santander Brasil | Development Products | APL - Release of Midrange Components"></td></tr><tr class="MenuTableRow"><td class="MenuEntryNameHover" nowrap="">PaaS | Configuração de Variáveis</td><td class="MenuEntryNoSubHover" arvalue="Santander Brasil | Cloud Services | PaaS Support - Setting up Rundeck PaaS JOBs"></td></tr><tr class="MenuTableRow"><td class="MenuEntryName" nowrap="">PaaS | Criação de Senha de Banco de Dados</td><td class="MenuEntryNoSub" arvalue="Santander Brasil | Cloud Services | PaaS Support - Project - Secrets Creation"></td></tr><tr class="MenuTableRow"><td class="MenuEntryName" nowrap="">Banco de Dados | Oracle | Validação de Script</td><td class="MenuEntryNoSub" arvalue="Santander Brasil | Database | APL - Oracle - Certification of Data Base"></td></tr><tr class="MenuTableRow"><td class="MenuEntryName" nowrap="">Segurança | Cerimônia de Senhas</td><td class="MenuEntryNoSub" arvalue="Santander Brasil | Systems (Access Management) | APL - Connect User - Maintenance"></td></tr>');
+				var menuStrConst = '<tr class="MenuTableRow"><td class="MenuEntryName" nowrap="">{nome}</td><td class="MenuEntryNoSub" arvalue="{valor}"></td></tr>';
+				var menuStr = "";
+				for (var i = 0; i < remedyoptions.length; i++) {
+					menuStr = menuStr + menuStrConst.replace("{nome}", remedyoptions[i].nome).replace("{valor}", remedyoptions[i].valor);
+				}
+				$("tbody.MenuTableBody").html(menuStr);
+				//$("tbody.MenuTableBody").html('<tr class="MenuTableRow"><td class="MenuEntryName" nowrap="">Operação | Release em Produção</td><td class="MenuEntryNoSub" arvalue="Santander Brasil | Development Products | APL - Release of Midrange Components"></td></tr><tr class="MenuTableRow"><td class="MenuEntryNameHover" nowrap="">PaaS | Configuração de Variáveis</td><td class="MenuEntryNoSubHover" arvalue="Santander Brasil | Cloud Services | PaaS Support - Setting up Rundeck PaaS JOBs"></td></tr><tr class="MenuTableRow"><td class="MenuEntryName" nowrap="">PaaS | Criação de Senha de Banco de Dados</td><td class="MenuEntryNoSub" arvalue="Santander Brasil | Cloud Services | PaaS Support - Project - Secrets Creation"></td></tr><tr class="MenuTableRow"><td class="MenuEntryName" nowrap="">Banco de Dados | Oracle | Validação de Script</td><td class="MenuEntryNoSub" arvalue="Santander Brasil | Database | APL - Oracle - Certification of Data Base"></td></tr><tr class="MenuTableRow"><td class="MenuEntryName" nowrap="">Segurança | Cerimônia de Senhas</td><td class="MenuEntryNoSub" arvalue="Santander Brasil | Systems (Access Management) | APL - Connect User - Maintenance"></td></tr>');
 				//alert('clicou');
 			}, 50);
 			
