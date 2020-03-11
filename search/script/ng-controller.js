eefb411c4d924d814c3ea060f78fe4b77977f6d7
Lucas Fabiano <lucasguaru@yahoo.com.br>
2020-03-11 20:43:29 -0300

    adicionando Dados e memorizador

--- search/script/ng-controller.js
+++ search/script/ng-controller.js
@@ -3,12 +3,15 @@ app.controller('HomeController', ['$scope', 'dados', function HomeController($sc
     let vm = this;
     vm.listaLinks = dados.pegar();
 
+    let x = 1;
+
     vm.adicionarItem = () => {
         vm.listaLinks = dados.adicionar({
-            titulo: 'Título 1',
+            titulo: 'Título ' + x,
             descricao: 'Reunimos as melhores práticas para criar o seu conteúdo com qualidade e rapidez, e tudo isso em um só lugar. Você ganha melhor visibilidade de seu site ou blog, geração automática de conteúdo rápido e de qualidade',
-            url: 'https://www.google.com/search?q=input+arredondado+css'
+            url: 'https://www.google.com/search?q=input+arredondado+css' + x
         });
+        x++;
     }
 
     // this.listaLinks = [{
