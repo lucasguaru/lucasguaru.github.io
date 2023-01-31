4ecba834b332e719ff8be9d2bf0a3b11b16af051
Lucas Fabiano <lucasguaru@yahoo.com.br>
2023-01-31 08:26:32 -0300

    removendo remedy

--- remedy-options.js
+++
@@ -1,7 +0,0 @@
-var remedyoptions = [
-    {nome: "Operação | Release em Produção", valor: "Santander Brasil | Development Products | APL - Release of Midrange Components"},
-    {nome: "PaaS | Configuração de Variáveis", valor: "Santander Brasil | Cloud Services | PaaS Support - Setting up Rundeck PaaS JOBs"},
-    {nome: "PaaS | Criação de Senha de Banco de Dados", valor: "Santander Brasil | Cloud Services | PaaS Support - Project - Secrets Creation"},
-    {nome: "Banco de Dados | Oracle | Validação de Script", valor: "Santander Brasil | Database | APL - Oracle - Certification of Data Base"},
-    {nome: "Segurança | Cerimônia de Senhas", valor: "Santander Brasil | Systems (Access Management) | APL - Connect User - Maintenance"}
-];
