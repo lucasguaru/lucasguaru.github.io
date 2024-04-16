9d55000487c4bbede5ebb96eb41dfdbecb526371
Lucas Fabiano <lucas.fabiano@fisoft.com.br>
2024-04-16 19:11:35 -0300

    quiz de provas de 2 ano 1 trime

---
+++ quiz-prova/script.js
@@ -0,0 +1,51 @@
+let questionList = [
+    "questions-portugues.json"
+];
+let chosenQuestion = questionList[Math.floor(Math.random() * questionList.length)];
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch(chosenQuestion)
+        .then(response => response.json())
+        .then(data => {
+            let currentQuestionIndex = 0;
+
+            function displayQuestion(questionData) {
+                const questionElement = document.getElementById('question');
+                const answersElement = document.getElementById('answers');
+
+                questionElement.textContent = (currentQuestionIndex + 1) + " - " + questionData.question;
+                answersElement.innerHTML = '';
+
+                const answers = questionData.answers;
+                const shuffledAnswers = answers.sort(() => Math.random() - 0.5);
+
+                shuffledAnswers.forEach(answer => {
+                    const answerElement = document.createElement('li');
+                    answerElement.textContent = answer;
+                    answerElement.addEventListener('click', () => {
+                        if (answer === questionData.correctAnswer) {
+                            currentQuestionIndex++;
+                            if (currentQuestionIndex < data.length) {
+                                displayQuestion(data[currentQuestionIndex]);
+                            } else {
+                                alert('Parabéns! Você completou o quiz!');
+                                currentQuestionIndex = 0;
+                                displayQuestion(data[currentQuestionIndex]);
+                            }
+                        } else {
+                            currentQuestionIndex = currentQuestionIndex -5;
+                            if (currentQuestionIndex < 0) currentQuestionIndex = 0;
+                            alert(`Resposta errada! Retorne a pergunta ${currentQuestionIndex + 1}.`);
+                            displayQuestion(data[currentQuestionIndex]);
+                        }
+                    });
+                    answersElement.appendChild(answerElement);
+                });
+            }
+
+            if (data.length > 0) {
+                displayQuestion(data[currentQuestionIndex]);
+            }
+        })
+        .catch(error => console.error('Erro ao carregar as perguntas:', error));
+});
